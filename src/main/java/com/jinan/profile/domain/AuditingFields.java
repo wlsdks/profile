cb93386aaae966c9a5c8997cd45f44bb173e39e0
wlsdks <wlsdks12@naver.com>
2023-07-22 00:49:19 +0900

    1. 기본적인 틀잡기 완료

--- src/main/java/com/jinan/profile/domain/AuditingFields.java
+++ src/main/java/com/jinan/profile/domain/AuditingFields.java
@@ -5,9 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -25,17 +23,17 @@ public abstract class AuditingFields {
     @Column(nullable = false, updatable = false)
     protected LocalDateTime createdAt; // 생성일시
 
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    protected String createdBy; // 생성자
-
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
     protected LocalDateTime modifiedAt; //수정일시
 
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    protected String modifiedBy; // 수정자
+//    @CreatedBy
+//    @Column(nullable = false, length = 100)
+//    protected String createdBy; // 생성자
+
+//    @LastModifiedBy
+//    @Column(nullable = false, length = 100)
+//    protected String modifiedBy; // 수정자
 
 }
