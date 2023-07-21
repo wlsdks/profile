5cb5cac0c0a61d4fa802a7356d6131ebb85ec2bc
wlsdks <wlsdks12@naver.com>
2023-07-22 01:05:32 +0900

    1.UserService코드 기본틀 추가

--- src/main/java/com/jinan/profile/service/UserService.java
+++ src/main/java/com/jinan/profile/service/UserService.java
@@ -1,4 +1,17 @@
 package com.jinan.profile.service;
 
+import com.jinan.profile.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    
+
 }
