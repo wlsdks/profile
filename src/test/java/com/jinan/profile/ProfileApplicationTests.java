7fe4890033038a7b11382172e3d59b364286fb68
wlsdks <wlsdks12@naver.com>
2023-07-21 23:14:26 +0900

    1. test코드 세팅
    테스트 코드를 동작시켜보려고 했는데 db관련 문제가 존재한다.
    지금 spring 3.1.1을 사용중인데 inmemory h2에 문제가 있는가보다

--- src/test/java/com/jinan/profile/ProfileApplicationTests.java
+++ src/test/java/com/jinan/profile/ProfileApplicationTests.java
@@ -2,12 +2,16 @@ package com.jinan.profile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 class ProfileApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("print test");
 	}
+
 
 }
