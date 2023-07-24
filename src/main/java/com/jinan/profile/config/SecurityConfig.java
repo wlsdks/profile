e04dad25f91b1989ee7031993defa9bf441cfa20
wlsdks12 <wlsdks12@naver.com>
2023-07-24 14:56:08 +0900

    1. 스프링 시큐리티 @Configuration클래스 수정
    1. 클래스 내부에서 userDetailsService를 @Bean으로 등록해줌 이걸로 유저를 찾아보고 로그인 기능이 동작하도록 해주더라.
    2. password인코더 구현 -> password인코더가 없으니까 로그인하면 바로 인코딩 에러가 나와서 passwordEncoder를 구현했다.

--- src/main/java/com/jinan/profile/config/SecurityConfig.java
+++ src/main/java/com/jinan/profile/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.jinan.profile.config;
 
+import com.jinan.profile.dto.security.Principal;
+import com.jinan.profile.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
@@ -22,21 +29,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(
             HttpSecurity http
-    ) throws Exception
-    {
-        http
-            .authorizeHttpRequests((authorize) -> authorize
-                    .requestMatchers("/resources/**").permitAll()         // "/resources/**" 경로에 대한 모든 요청을 허용한다.
-                    .requestMatchers("/admin/**").access(this::isAdmin)   // "/admin/**" 경로에 대한 요청은 "ADMIN" 역할을 가진 사용자만 접근할 수 있게한다.
+    ) throws Exception {
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                                .requestMatchers("/resources/**").permitAll()         // "/resources/**" 경로에 대한 모든 요청을 허용한다.
+                                .requestMatchers("/admin/**").access(this::isAdmin)   // "/admin/**" 경로에 대한 요청은 "ADMIN" 역할을 가진 사용자만 접근할 수 있게한다.
+                                .requestMatchers(
+                                        HttpMethod.GET,
+                                        "/"
+                                ).permitAll()
 //                    .requestMatchers("/").hasRole("USER")                 // 또한, "/" 경로는 "USER" 역할을 가진 사용자만 접근할 수 있도록 설정하였다.
-                    .anyRequest().authenticated()                           // 그 외의 모든 요청은 인증된 사용자만 접근할 수 있도록 설정하였다.
-            )
-            .formLogin(Customizer.withDefaults())
-            .logout(logout -> logout
-                    .logoutSuccessUrl("/")
-            );
-
-        return http.build();
+                                .anyRequest().authenticated()                           // 그 외의 모든 요청은 인증된 사용자만 접근할 수 있도록 설정하였다.
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                ).build();
     }
 
     /**
@@ -45,13 +53,34 @@ public class SecurityConfig {
      */
     private AuthorizationDecision isAdmin(
             Supplier<Authentication> authenticationSupplier,
-            RequestAuthorizationContext requestAuthorizationContext)
-    {
+            RequestAuthorizationContext requestAuthorizationContext) {
         return new AuthorizationDecision(
                 authenticationSupplier.get()
                         .getAuthorities()
                         .contains(new SimpleGrantedAuthority("ADMIN"))
         );
     }
+
+    /**
+     * 이녀석은 db를 기반으로 인증정보를 불러와서 user정보를 return하는 역할을 한다.
+     * OAuth2에도 이런역할 하는게 필요한데 이걸 사용할수는 없어서 따로 만들어서 @Bean으로 등록해준다.
+     */
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService) {
+        return username -> userService
+                .searchUser(username)
+                .map(Principal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - userId: " + username));
+    }
+
+    /**
+     * password 인코더 구현
+     * spring security 암호화 모듈을 사용한다.
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }
