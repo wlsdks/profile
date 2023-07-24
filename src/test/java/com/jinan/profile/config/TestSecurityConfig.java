ae8c1dcd73e7e490afd8d5bd266f4efc925e5356
wlsdks12 <wlsdks12@naver.com>
2023-07-24 16:15:28 +0900

    1. data.sql 비밀번호 암호화로 인한 내용 수정

---
+++ src/test/java/com/jinan/profile/config/TestSecurityConfig.java
@@ -0,0 +1,2 @@
+package com.jinan.profile.config;public class TestSecurityConfig {
+}
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO USERS (username, email, password, role, created_at, updated_at)
-VALUES ('wlsdks12', 'wlsdks12@naver.com', 'wlsdks12', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('user2', 'user2@email.com', 'password2', 'USER', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('user3', 'user3@email.com', 'password3', 'ROLE_USER', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('wlsdks12', 'wlsdks12@naver.com', '{noop}wlsdks12', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('user2', 'user2@email.com', '{noop}password2', 'USER', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('user3', 'user3@email.com', '{noop}password3', 'ROLE_USER', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
