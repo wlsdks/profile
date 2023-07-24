0e31dc6c9fdcc5da7abd3f0a64f56b6507c0b80e
wlsdks12 <wlsdks12@naver.com>
2023-07-24 14:55:04 +0900

    1. 서비스에서 찾아온 User를 UserDto로 변환하고 반환하는 코드 작성

--- src/main/java/com/jinan/profile/service/UserService.java
+++ src/main/java/com/jinan/profile/service/UserService.java
@@ -1,16 +1,30 @@
 package com.jinan.profile.service;
 
+import com.jinan.profile.dto.UsersDto;
 import com.jinan.profile.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    /**
+     * repository에서 가져오는 entity정보는 service 외부로는 나가지 않도록 dto를 사용한다.(격리를 시킨다.)
+     * 이 메소드에서 마지막에 orElse 예외처리를 해주지 않은것은 이 service 레이어에서 에러처리를 전담하지않기 위함이다.(상위로 전파)
+     */
+    public Optional<UsersDto> searchUser(String username) {
+        return userRepository.findByUsername(username)
+                .map(UsersDto::from);
+    }
 
 
 
