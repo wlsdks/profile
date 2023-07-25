619352b95a9d1edd18daeafdc4f653481e79d758
wlsdks12 <wlsdks12@naver.com>
2023-07-25 13:58:38 +0900

    1. 대댓글 신고 dto생성완료

---
+++ src/main/java/com/jinan/profile/dto/board/BoardSubCommentReportDto.java
@@ -0,0 +1,48 @@
+package com.jinan.profile.dto.board;
+
+import com.jinan.profile.domain.board.BoardSubComment;
+import com.jinan.profile.domain.board.BoardSubCommentReport;
+import com.jinan.profile.dto.user.UsersDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * DTO for {@link com.jinan.profile.domain.board.BoardSubCommentReport}
+ */
+public record BoardSubCommentReportDto(
+        Long id,
+        String reason,
+        List<UsersDto> subCommentReporter,
+        UsersDto subCommentReported,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+
+    // 생성자 factory method of 선언
+    public static BoardSubCommentReportDto of(Long id, String reason, List<UsersDto> subCommentReporter, UsersDto subCommentReported, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        return new BoardSubCommentReportDto(id, reason, subCommentReporter, subCommentReported, createdAt, updatedAt);
+    }
+
+    // entity를  받아서 dto로 변환하는 코드
+    public static BoardSubCommentReportDto fromEntity(BoardSubCommentReport entity) {
+
+        List<UsersDto> subCommentReporterList = Optional.ofNullable(entity.getSubCommentReporter())
+                .stream()
+                .map(UsersDto::fromEntity)
+                .collect(Collectors.toList());
+
+        return new BoardSubCommentReportDto(
+                entity.getId(),
+                entity.getReason(),
+                subCommentReporterList,
+                UsersDto.fromEntity(entity.getSubCommentReported()),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
+    }
+
+
+}
