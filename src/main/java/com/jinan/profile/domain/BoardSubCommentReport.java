dc7ab1c7e36102deb9fe0eda6766796a14e3b361
wlsdks12 <wlsdks12@naver.com>
2023-07-25 11:04:41 +0900

    1. 대댓글 엔티티설계완료 및 유저엔티티 연관관계 추가

--- src/main/java/com/jinan/profile/domain/BoardSubCommentReport.java
+++ src/main/java/com/jinan/profile/domain/BoardSubCommentReport.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @ToString(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -17,5 +19,48 @@ public class BoardSubCommentReport {
     @Column(name = "board_sub_comment_report_id")
     private Long id;
 
+    @ToString.Exclude
+    @JoinColumn(name = "board_sub_reporter_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Users subCommentReporter;
+
+    @ToString.Exclude
+    @JoinColumn(name = "board_sub_reported_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Users subCommentReported;
+
+    @ToString.Exclude
+    @JoinColumn(name = "board_sub_comment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private BoardSubComment boardSubComment;
+
+    @Column(name = "reason")
+    private String reason;
+
+    // 생성자 선언
+    private BoardSubCommentReport(Users subCommentReporter, Users subCommentReported, BoardSubComment boardSubComment, String reason) {
+        this.subCommentReporter = subCommentReporter;
+        this.subCommentReported = subCommentReported;
+        this.boardSubComment = boardSubComment;
+        this.reason = reason;
+    }
+
+    // 생성자 factory method 선언
+    public BoardSubCommentReport of(Users subCommentReporter, Users subCommentReported, BoardSubComment boardSubComment, String reason) {
+        return new BoardSubCommentReport(subCommentReporter, subCommentReported, boardSubComment, reason);
+    }
+
+    // equals & hashCode 최적화
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BoardSubCommentReport that)) return false;
+        return that.id != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
 }
--- src/main/java/com/jinan/profile/domain/Users.java
+++ src/main/java/com/jinan/profile/domain/Users.java
@@ -67,6 +67,14 @@ public class Users extends AuditingFields {
     @OneToMany(mappedBy = "commentReported")
     private List<BoardCommentReport> commentReported = new ArrayList<>(); // 댓글 신고당한사람 리스트
 
+    @ToString.Exclude
+    @OneToMany(mappedBy = "subCommentReporter")
+    private List<BoardSubCommentReport> subCommentReporter = new ArrayList<>(); // 대댓글 신고자 리스트
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "subCommentReported")
+    private List<BoardSubCommentReport> subCommentReported = new ArrayList<>(); // 대댓글 신고당한사람 리스트
+
 
     // id, 생성일자, 수정일자는 자동으로 등록된다.
     private Users(String username, String email, String password, RoleType roleType) {
