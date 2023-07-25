ebe34323b92fe6427d57eecc4fdf0d31c121ac29
wlsdks12 <wlsdks12@naver.com>
2023-07-25 16:09:43 +0900

    1. 채팅관련 모든 dto선언완료

---
+++ src/main/java/com/jinan/profile/dto/message/ChatRoomDto.java
@@ -0,0 +1,27 @@
+package com.jinan.profile.dto.message;
+
+import com.jinan.profile.domain.message.ChatRoom;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.jinan.profile.domain.message.ChatRoom}
+ */
+public record ChatRoomDto(
+        Long id,
+        String chatRoomName
+) {
+
+    // factory method of 선언
+    public static ChatRoomDto of(Long id, String chatRoomName) {
+        return new ChatRoomDto(id, chatRoomName);
+    }
+
+    // entity를 dto로 변환하는 factory 메서드
+    public static ChatRoomDto fromEntity(ChatRoom entity) {
+        return ChatRoomDto.of(
+                entity.getId(),
+                entity.getChatRoomName()
+        );
+    }
+}
---
+++ src/main/java/com/jinan/profile/dto/message/MessageDto.java
@@ -0,0 +1,36 @@
+package com.jinan.profile.dto.message;
+
+import com.jinan.profile.domain.message.Message;
+import com.jinan.profile.dto.user.UsersDto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link com.jinan.profile.domain.message.Message}
+ */
+public record MessageDto(
+        Long id,
+        UsersDto users,
+        ChatRoomDto chatRoom,
+        String text,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+
+    // 생성자 factory method 선언
+    public static MessageDto of(Long id, UsersDto users, ChatRoomDto chatRoom, String text, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        return new MessageDto(id, users, chatRoom, text, createdAt, updatedAt);
+    }
+
+    // entity를 dto로 변환하는 메서드
+    public static MessageDto fromEntity(Message entity) {
+        return MessageDto.of(
+                entity.getId(),
+                UsersDto.fromEntity(entity.getUsers()),
+                ChatRoomDto.fromEntity(entity.getChatRoom()),
+                entity.getText(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
+    }
+}
