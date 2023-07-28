d69b38335eb723324b25ed33c1640d56f2d20bb2
wlsdks12 <wlsdks12@naver.com>
2023-07-28 09:32:44 +0900

    1. 기초적인 웹소켓 통신 연결 및 테스트 완료

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,60 +1,61 @@
-const stompClient = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/gs-guide-websocket'
-});
+//stomp와 sockJS관련 임포트 혹은 html에서 스크립트를 넣어야한다
+let socket = new SockJS('/ws');
+let stompClient = Stomp.over(socket);
 
-stompClient.onConnect = (frame) => {
+// 웹소켓 최초 연결함수
+stompClient.connect({}, onConnected, onError);
+
+// 웹소켓 연결
+function onConnected() {
     setConnected(true);
-    console.log('Connected: ' + frame);
+    // console.log('Connected: ' + frame);
     stompClient.subscribe('/topic/greetings', (greeting) => {
         showGreeting(JSON.parse(greeting.body).content);
     });
-};
+}
 
-stompClient.onWebSocketError = (error) => {
+// 에러 함수
+function onError(error) {
     console.error('Error with websocket', error);
-};
+}
 
-stompClient.onStompError = (frame) => {
-    console.error('Broker reported error: ' + frame.headers['message']);
-    console.error('Additional details: ' + frame.body);
-};
-
+// 연결세팅하는 함수
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
         $("#conversation").show();
-    }
-    else {
+    } else {
         $("#conversation").hide();
     }
     $("#greetings").html("");
 }
 
-function connect() {
-    stompClient.activate();
-}
-
+// 웹소켓 연결 해제
 function disconnect() {
-    stompClient.deactivate();
+    stompClient.disconnect();
     setConnected(false);
     console.log("Disconnected");
 }
 
+// 이름 전송
 function sendName() {
-    stompClient.publish({
-        destination: "/app/hello",
-        body: JSON.stringify({'name': $("#name").val()})
-    });
+    stompClient.send(
+        "/app/hello",
+        {},
+        JSON.stringify({'name': $("#name").val()})
+    );
 }
 
+// 인사메시지 보여주기
 function showGreeting(message) {
     $("#greetings").append("<tr><td>" + message + "</td></tr>");
 }
 
+// 폼 제출 함수
 $(function () {
     $("form").on('submit', (e) => e.preventDefault());
-    $( "#connect" ).click(() => connect());
-    $( "#disconnect" ).click(() => disconnect());
-    $( "#send" ).click(() => sendName());
+    $("#connect").click(() => connect());
+    $("#disconnect").click(() => disconnect());
+    $("#send").click(() => sendName());
 });
