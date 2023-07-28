6772f74a3c64516d1185dd4eb3522fa712964447
wlsdks <wlsdks12@naver.com>
2023-07-29 00:09:17 +0900

    메시지 리포지토리 생성

---
+++ src/main/java/com/jinan/profile/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.jinan.profile.repository;
+
+import com.jinan.profile.domain.message.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByChatroomId(Long chatroomId);
+}
