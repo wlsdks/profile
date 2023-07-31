8b01c7fdd8fb0cb1f7edf6704432b44d46efce88
wlsdks12 <wlsdks12@naver.com>
2023-07-31 10:39:54 +0900

    1. 테스트코드가 돌아가지않던부분 수정

--- src/test/java/com/jinan/profile/ProfileApplicationTests.java
+++ src/test/java/com/jinan/profile/ProfileApplicationTests.java
@@ -1,10 +1,13 @@
 package com.jinan.profile;
 
+import com.jinan.profile.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Profile("test")
+@Import(TestSecurityConfig.class)
 @SpringBootTest
 class ProfileApplicationTests {
 
