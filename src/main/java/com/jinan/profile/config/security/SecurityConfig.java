c8ed0fb22eae4bca1792d43f8da697f88e8c5847
wlsdks12 <wlsdks12@naver.com>
2023-08-03 08:28:29 +0900

    1. 시큐리티 동작 설명주석 추가, 리팩토링 진행

--- src/main/java/com/jinan/profile/config/security/SecurityConfig.java
+++ src/main/java/com/jinan/profile/config/security/SecurityConfig.java
@@ -73,11 +73,9 @@ public class SecurityConfig {
                         .anyRequest().authenticated()                           // 그 외의 모든 요청은 인증된 사용자만 접근할 수 있도록 설정하였다.
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(jwtAuthorizationFilter, BasicAuthenticationFilter.class) // jwtAuthorizationFilter() 직접 작성해야함
                 .formLogin(AbstractHttpConfigurer::disable)
-//            .formLogin(Customizer.withDefaults())
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // customAuthenticationFilter 직접 작성해야함
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
@@ -85,36 +83,7 @@ public class SecurityConfig {
     }
 
     /**
-     * authenticate 의 인증 메서드를 제공하는 매니져로'Provider'의 인터페이스를 의미한다.
-     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
-     */
-    @Bean
-    public AuthenticationManager authenticationManager(CustomAuthenticationProvider customAuthenticationProvider) {
-        return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
-    }
-
-    /**
-     * '인증' 제공자로 사용자의 이름과 비밀번호가 요구된다.
-     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
-     */
-    @Bean
-    public CustomAuthenticationProvider customAuthenticationProvider(SecurityUserService securityUserService) {
-        return new CustomAuthenticationProvider(
-                userDetailsService(securityUserService),
-                bCryptPasswordEncoder()
-        );
-    }
-
-    /**
-     * 비밀번호를 암호화하기 위한 BCrypt 인코딩을 통하여 비밀번호에 대한 암호화를 수행한다.
-     */
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    /**
-     * 6. 커스텀을 수행한 '인증' 필터로 접근 URL, 데이터 전달방식(form) 등 인증 과정 및 인증 후 처리에 대한 설정을 구성하는 메서드입니다.
-     *
+     * 1. 커스텀을 수행한 '인증' 필터로 접근 URL, 데이터 전달방식(form) 등 인증 과정 및 인증 후 처리에 대한 설정을 구성하는 메서드다.
      * @return CustomAuthenticationFilter
      */
     @Bean
@@ -128,66 +97,28 @@ public class SecurityConfig {
     }
 
     /**
-     * 7. Spring Security 기반의 사용자의 정보가 맞을 경우 수행이 되며 결과값을 리턴해주는 Handler
-     *
-     * @return CustomLoginSuccessHandler
+     * 2. authenticate 의 인증 메서드를 제공하는 매니져로'Provider'의 인터페이스를 의미한다.
+     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
      */
     @Bean
-    public CustomAuthSuccessHandler customLoginSuccessHandler() {
-        return new CustomAuthSuccessHandler();
+    public AuthenticationManager authenticationManager(CustomAuthenticationProvider customAuthenticationProvider) {
+        return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
     }
 
     /**
-     * 8. Spring Security 기반의 사용자의 정보가 맞지 않을 경우 수행이 되며 결과값을 리턴해주는 Handler
-     *
-     * @return CustomAuthFailureHandler
+     * 3. '인증' 제공자로 사용자의 이름과 비밀번호가 요구된다.
+     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
      */
     @Bean
-    public CustomAuthFailureHandler customLoginFailureHandler() {
-        return new CustomAuthFailureHandler();
-    }
-
-    /**
-     * 9. JWT 토큰을 통하여서 사용자를 인증합니다.
-     *
-     * @return JwtAuthorizationFilter
-     */
-    @Bean
-    public JwtAuthorizationFilter jwtAuthorizationFilter() {
-        return new JwtAuthorizationFilter();
-    }
-
-    /**
-     * isAdmin 메소드는 Supplier<Authentication>와 RequestAuthorizationContext를 인자로 받아서 "ADMIN" 역할을 가진 사용자인지 확인한다.
-     * 만약 사용자가 "ADMIN" 역할을 가지고 있다면, AuthorizationDecision 객체는 true를 반환하고, 그렇지 않다면 false를 반환한다.
-     */
-    private AuthorizationDecision isAdmin(
-            Supplier<Authentication> authenticationSupplier,
-            RequestAuthorizationContext requestAuthorizationContext
-    ) {
-        return new AuthorizationDecision(
-                authenticationSupplier.get()
-                        .getAuthorities()
-                        .contains(new SimpleGrantedAuthority("ADMIN"))
+    public CustomAuthenticationProvider customAuthenticationProvider(SecurityUserService securityUserService) {
+        return new CustomAuthenticationProvider(
+                userDetailsService(securityUserService),
+                bCryptPasswordEncoder()
         );
     }
 
     /**
-     * 유저정보를 받아오는 메서드
-     * @param userRepository
-     * @return UserDto
-     */
-    @Bean
-    public SecurityUserService securityUserService(UserRepository userRepository) {
-        return userDto -> userRepository
-                .findByUsername(userDto.username())
-                .map(UserDto::fromEntity);
-    }
-
-    /**
-     * 시큐리티의 UserDetailsService를 구현한 메서드 사용자 정보를 통해 구분해서 로그인했는지 안했는지 판단한다.
-     * @param securityUserService
-     * @return
+     * 3-1. 시큐리티의 UserDetailsService를 구현한 메서드 사용자 정보를 통해 구분해서 로그인했는지 안했는지 판단한다.
      */
     @Bean
     public UserDetailsService userDetailsService(SecurityUserService securityUserService) {
@@ -211,6 +142,61 @@ public class SecurityConfig {
         };
     }
 
+    /**
+     * 3-2. 유저정보를 받아오는 메서드
+     */
+    @Bean
+    public SecurityUserService securityUserService(UserRepository userRepository) {
+        return userDto -> userRepository
+                .findByUsername(userDto.username())
+                .map(UserDto::fromEntity);
+    }
+
+    /**
+     * 3-3. 비밀번호를 암호화하기 위한 BCrypt 인코딩을 통하여 비밀번호에 대한 암호화를 수행한다.
+     */
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 4. Spring Security 기반의 사용자의 정보가 맞을 경우 수행이 되며 결과값을 리턴해주는 Handler
+     */
+    @Bean
+    public CustomAuthSuccessHandler customLoginSuccessHandler() {
+        return new CustomAuthSuccessHandler();
+    }
+
+    /**
+     * 5. Spring Security 기반의 사용자의 정보가 맞지 않을 경우 수행이 되며 결과값을 리턴해주는 Handler
+     */
+    @Bean
+    public CustomAuthFailureHandler customLoginFailureHandler() {
+        return new CustomAuthFailureHandler();
+    }
+
+    /**
+     * JWT 토큰을 통하여서 사용자를 인증합니다.
+     */
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter();
+    }
+
+    /**
+     * isAdmin 메소드는 Supplier<Authentication>와 RequestAuthorizationContext를 인자로 받아서 "ADMIN" 역할을 가진 사용자인지 확인한다.
+     * 만약 사용자가 "ADMIN" 역할을 가지고 있다면, AuthorizationDecision 객체는 true를 반환하고, 그렇지 않다면 false를 반환한다.
+     */
+    private AuthorizationDecision isAdmin(
+            Supplier<Authentication> authenticationSupplier,
+            RequestAuthorizationContext requestAuthorizationContext
+    ) {
+        return new AuthorizationDecision(
+                authenticationSupplier.get()
+                        .getAuthorities()
+                        .contains(new SimpleGrantedAuthority("ADMIN"))
+        );
+    }
 
 
 }
--- src/main/java/com/jinan/profile/config/security/custom/CustomAuthenticationProvider.java
+++ src/main/java/com/jinan/profile/config/security/custom/CustomAuthenticationProvider.java
@@ -21,10 +21,21 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder passwordEncoder;
 
+    /**
+     * 커스텀 AuthenticationProvider를 구현한 메서드
+     * 사용자 인증을 처리한다.
+     * 사용자가 제공한 인증 정보(여기서는 사용자 이름과 비밀번호)를 받아서 검증하고, 인증이 성공하면 인증된 사용자의 정보를 담은 Authentication 객체를 반환한다.
+     * @throws AuthenticationException
+     */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         log.debug("2.CustomAuthenticationProvider");
 
+        /**
+         * UsernamePasswordAuthenticationToken은 Spring Security에서 제공하는 Authentication 구현체 중 하나로,
+         * 사용자 이름과 비밀번호를 기반으로 인증을 처리하는데 사용된다.
+         * 이 클래스의 인스턴스는 사용자가 로그인 폼에서 제출한 사용자 이름과 비밀번호를 담고 있다.
+         */
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
 
         // AuthenticationFilter에서 생성된 토큰으로부터 ID, PW를 조회
@@ -37,9 +48,18 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         if (!(securityUserDetailsDto.getPassword().equalsIgnoreCase(userPassword) && securityUserDetailsDto.getPassword().equalsIgnoreCase(userPassword))) {
             throw new BadCredentialsException(securityUserDetailsDto.getUsername() + "Invalid password");
         }
+
+        /**
+         * 인증이 성공하면 인증된 사용자의 정보와 권한을 담은 새로운 UsernamePasswordAuthenticationToken을 반환한다.
+         */
         return new UsernamePasswordAuthenticationToken(securityUserDetailsDto, userPassword, securityUserDetailsDto.getAuthorities());
     }
 
+    /**
+     * 이 AuthenticationProvider가 특정 Authentication 타입을 지원하는지 여부를 반환한다.
+     * 여기서는 UsernamePasswordAuthenticationToken 클래스를 지원한다.
+     * @param authentication
+     */
     @Override
     public boolean supports(Class<?> authentication) {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
