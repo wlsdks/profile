8f8d2e13d571bb5bd6931ae0a92c5baa7728b2ac
wlsdks <wlsdks12@naver.com>
2023-08-06 18:47:04 +0900

    1. 코드추가

--- src/main/java/com/jinan/profile/config/SecurityConfig.java
+++ src/main/java/com/jinan/profile/config/SecurityConfig.java
@@ -1,29 +1,30 @@
 package com.jinan.profile.config;
 
-<<<<<<< HEAD
-=======
 import com.jinan.profile.config.security.filter.CustomAuthenticationFilter;
 import com.jinan.profile.config.security.filter.JwtAuthorizationFilter;
 import com.jinan.profile.config.security.handler.CustomAuthFailureHandler;
+import com.jinan.profile.config.security.handler.CustomAuthSuccessHandler;
 import com.jinan.profile.config.security.handler.CustomAuthenticationProvider;
 import com.jinan.profile.service.security.CustomUserDetailsService;
->>>>>>> main
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-<<<<<<< HEAD
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-=======
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -32,49 +33,47 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.function.Supplier;
->>>>>>> main
 
 @Slf4j
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+    /**
+     * 이 메서드는 정적 자원에 대해 보안을 적용하지 않도록 설정한다.
+     * 정적 자원은 보통 HTML, CSS, JavaScript, 이미지 파일 등을 의미하며, 이들에 대해 보안을 적용하지 않음으로써 성능을 향상시킬 수 있다.
+     */
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        // resources 자원 접근을 허용한다.
         return web -> web.ignoring()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+    /**
+     * Spring security6부터는 @Bean으로 filterChain을 등록하는 방식이 권장된다.
+     * 그리고 기존에 사용하던 문법인 anyMatchers, MvcRequestMatchers는 requestMatchers로 통일되었다.
+     * 1.csrf(disable): csrf비활성화 -> 2.authorizeHttpRequests: 특정경로 요청제어
+     * 3.sessionManagement(): SessionCreationPolicy.STATELESS로 설정하면 Spring Security는 세션을 생성하지 않고, 인증을 위해 세션을 사용하지 않는다. 이는 주로 토큰 기반의 인증 방식에서 사용된다.
+     * 4.httpBasic(): HTTP Basic 인증을 활성화한다. HTTP Basic 인증은 클라이언트가 요청 헤더의 Authorization 필드에 사용자 이름과 비밀번호를 Base64 인코딩된 형태로 전송하는 인증 방식이다.
+     * 5 addFilterBefore(): 이 설정은 특정 필터를 다른 필터 앞에 추가한다. 여기서는 JwtAuthorizationFilter를 BasicAuthenticationFilter 앞에, CustomAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 추가했다.
+     * 이 필터들은 HTTP 요청이 들어올 때 실행되며, 요청을 처리하거나 다음 필터로 전달하거나, 요청을 거부할 수 있다.
+     * 6. formLogin(AbstractHttpConfigurer::disable): 이 설정은 폼 기반 로그인을 비활성화한다. 폼 기반 로그인은 사용자가 로그인 폼을 통해 사용자 이름과 비밀번호를 전송하는 인증 방식이다.
+     * 7. logout: 이 설정은 로그아웃 기능을 설정한다. 로그아웃 성공 시 "/"로 리다이렉트하도록 설정하였다.
+     *
+     * <p>기능 요약</p>
+     * 사용자가 로그인을 시도하면 CustomAuthenticationFilter가 이를 처리한다.
+     * 이 필터는 사용자가 제출한 사용자 이름과 비밀번호를 가지고 인증을 시도하고, 인증이 성공하면 인증된 사용자의 정보와 권한을 담은 Authentication 객체를 생성하여 SecurityContext에 저장한다.
+     * 이후 HTTP 요청이 들어오면 JwtAuthorizationFilter가 이를 처리하며, 이 필터는 요청 헤더의 JWT 토큰을 검증하고, 토큰이 유효하면 토큰에서 사용자의 정보와 권한을 추출하여 SecurityContext에 저장한다.
+     * 이렇게 SecurityContext에 저장된 사용자의 정보와 권한은 애플리케이션의 다른 부분에서 사용될 수 있다.
+     */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            CustomAuthenticationFilter customAuthenticationFilter,
+            JwtAuthorizationFilter jwtAuthorizationFilter
+    ) throws Exception {
         log.debug("[+] WebSecurityConfig Start !!! ");
-        // csrf 설정 해제
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-<<<<<<< HEAD
-                .authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers(HttpMethod.GET, "/user/signup").permitAll() // 회원가입 페이지 허용
-                        .requestMatchers("/login").permitAll() // 로그인 페이지 허용
-//                        .requestMatchers(HttpMethod.GET, "/user/api/signup").permitAll() // 회원가입 요청도 무조건 허용
-//                        .requestMatchers(HttpMethod.GET, "/user/api/login").permitAll() // 로그인 요청도 무조건 허용
-//                        .requestMatchers("/cookie/test").permitAll()
-//                        .requestMatchers("/user/vip").hasRole(UserRoleEnum.VIP_MEMBER.toString()) // vip멤버 전용 페이지
-//                        .requestMatchers("/user/admin").hasRole(UserRoleEnum.ADMIN.toString()) // 관리자 전용 페이지
-                        .anyRequest().authenticated() // 나머지 요청은 전부 인증되어야 접속가능하다.
-                ) // 회원가입 페이지는 무조건 허용한다.
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin(login -> login.loginPage("/login").permitAll())
-//                .exceptionHandling(exception -> exception.accessDeniedPage("/forbidden"))
-                .build();
-    }
-
-=======
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/resources/**").permitAll()
@@ -198,7 +197,6 @@ public class SecurityConfig {
     }
 
 
->>>>>>> main
 }
 
 
