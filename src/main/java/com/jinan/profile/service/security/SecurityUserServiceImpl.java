59ae354e45b7bf2e13f103acc2d62121322d63fe
wlsdks <wlsdks12@naver.com>
2023-08-06 18:48:54 +0900

    1. 코드추가

---
+++ src/main/java/com/jinan/profile/service/security/SecurityUserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jinan.profile.service.security;
+
+import com.jinan.profile.dto.user.UserDto;
+import com.jinan.profile.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SecurityUserServiceImpl implements SecurityUserService{
+
+    private final UserRepository userRepository;
+
+    // login하면 Optional<UserDto>를 반환한다.
+    @Override
+    public Optional<UserDto> login(UserDto userDto) {
+        return userRepository.findByLoginId(userDto.loginId())
+                .map(UserDto::fromEntity);
+    }
+
+}
