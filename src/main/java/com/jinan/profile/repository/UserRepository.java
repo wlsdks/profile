bd86a0f01fa1d9ca0b78621910f6897c9f3f76f7
wlsdks <wlsdks12@naver.com>
2023-08-06 18:55:57 +0900

    1. 코드수정

--- src/main/java/com/jinan/profile/repository/UserRepository.java
+++ src/main/java/com/jinan/profile/repository/UserRepository.java
@@ -3,20 +3,14 @@ package com.jinan.profile.repository;
 import com.jinan.profile.domain.user.User;
 import com.jinan.profile.repository.querydsl.UserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 //querydsl custom 인터페이스로 extends에 추가해준다.
-@Repository
 public interface UserRepository extends
         JpaRepository<User, Long>
         , UserRepositoryCustom
 {
     Optional<User> findByUsername(String username);
-<<<<<<< HEAD
-    Optional<User> findByLoginId(String loginId);
-=======
     Optional<User> findByLoginId(String username);
->>>>>>> main
 }
