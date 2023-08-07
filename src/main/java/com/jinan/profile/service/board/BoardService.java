9c836c59ed62484a6992181bae1b7a467806b7ce
wlsdks12 <wlsdks12@naver.com>
2023-08-07 15:04:00 +0900

    1. 게시글을 저장할땐 무조건 유저가 필요하다고 가정하고 예외케이스를 테스트했다.

--- src/main/java/com/jinan/profile/service/board/BoardService.java
+++ src/main/java/com/jinan/profile/service/board/BoardService.java
@@ -8,20 +8,25 @@ import com.jinan.profile.repository.board.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class BoardService {
 
     private final BoardRepository boardRepository;
 
     /**
-     * 게시글을 저장한다.
+     * 게시글을 저장한다. -> 게시글을 작성한 유저는 무조건 있어야 한다.
      */
     public BoardDto saveBoard(Board board) {
+        if (board.getUser() == null) {
+            throw new ProfileApplicationException(ErrorCode.USER_NOT_FOUND);
+        }
         return Optional.of(boardRepository.save(board))
                 .map(BoardDto::fromEntity)
                 .orElseThrow(() -> new ProfileApplicationException(ErrorCode.INSERT_ERROR));
--- src/test/java/com/jinan/profile/service/board/BoardServiceTest.java
+++ src/test/java/com/jinan/profile/service/board/BoardServiceTest.java
@@ -5,10 +5,13 @@ import com.jinan.profile.domain.user.User;
 import com.jinan.profile.domain.user.constant.RoleType;
 import com.jinan.profile.domain.user.constant.UserStatus;
 import com.jinan.profile.dto.board.BoardDto;
+import com.jinan.profile.exception.ErrorCode;
+import com.jinan.profile.exception.ProfileApplicationException;
 import com.jinan.profile.repository.board.BoardRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +20,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 @DisplayName("[Board] - 서비스 레이어 테스트")
 @Transactional
@@ -32,7 +36,7 @@ class BoardServiceTest {
 
     @DisplayName("유저가 작성한 게시글을 저장한다.")
     @Test
-    void test() {
+    void saveBoard() {
         //given
         User user = createUser();
         Board board = createBoard(user);
@@ -43,6 +47,28 @@ class BoardServiceTest {
         //then
         assertThat(boardDto).isNotNull();
         assertThat(boardDto).hasFieldOrPropertyWithValue("title", "테스트 게시글");
+        assertThat(boardDto).hasFieldOrPropertyWithValue("content", "테스트");
+        assertThat(boardDto.title()).isEqualTo("테스트 게시글");
+        assertThat(boardDto.content()).isEqualTo("테스트");
+        assertThat(boardDto).isInstanceOf(BoardDto.class);
+
+
+        // 만약 컬렉션이 있다면
+//        assertThat(boardDto.getComments()).hasSize(0);
+//        assertThat(boardDto.getComments()).contains(commentDto);
+//        assertThat(boardDto.getComments()).isEmpty();
+    }
+
+    @DisplayName("유저가 지정되지 않은 게시글을 저장하면 예외가 발생한다.")
+    @Test
+    void test() {
+        //given
+        User user = createUser();
+        Board board = createBoard(null);
+
+        //when & then
+        assertThatThrownBy(() -> boardService.saveBoard(board))
+                .isInstanceOf(ProfileApplicationException.class);
 
     }
 
