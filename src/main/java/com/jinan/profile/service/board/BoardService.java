546e2539f23137d8915c9ecb14ede9566aabdee0
wlsdks12 <wlsdks12@naver.com>
2023-08-07 14:27:50 +0900

    1. board 서비스 작성

---
+++ src/main/java/com/jinan/profile/service/board/BoardService.java
@@ -0,0 +1,14 @@
+package com.jinan.profile.service.board;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+
+
+}
