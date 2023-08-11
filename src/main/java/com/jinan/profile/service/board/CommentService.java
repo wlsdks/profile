bb769ab7cdb006294cb899f601b871caca25c52d
wlsdks12 <wlsdks12@naver.com>
2023-08-11 10:49:14 +0900

    1. 만든것들을 사용해서 댓글 서비스를 작성함 - 테스트가 필요하다.

--- src/main/java/com/jinan/profile/service/board/CommentService.java
+++ src/main/java/com/jinan/profile/service/board/CommentService.java
@@ -1,12 +1,18 @@
 package com.jinan.profile.service.board;
 
 import com.jinan.profile.controller.board.request.BoardCommentRequest;
+import com.jinan.profile.domain.board.BoardComment;
 import com.jinan.profile.dto.board.BoardCommentDto;
+import com.jinan.profile.exception.ErrorCode;
+import com.jinan.profile.exception.ProfileApplicationException;
 import com.jinan.profile.repository.board.BoardRepository;
+import com.jinan.profile.repository.board.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Transactional
@@ -14,12 +20,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommentService {
 
-    private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
 
     /**
      * 유저가 댓글을 작성하면 저장된다.
      */
     public BoardCommentDto createComment(BoardCommentRequest request) {
+
+        BoardComment boardComment = Optional.of(request)
+                .map(BoardCommentDto::fromRequest)
+                .map(BoardComment::fromDto)
+                .orElseThrow(() -> new ProfileApplicationException(ErrorCode.USER_NOT_FOUND));
+
+        BoardComment savedComment = commentRepository.save(boardComment);
+
         return null;
     }
 
