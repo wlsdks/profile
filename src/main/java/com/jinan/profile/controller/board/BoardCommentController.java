17cdffb8f30e8979d2ec8039d3a48ac3851c76b7
wlsdks12 <wlsdks12@naver.com>
2023-08-11 15:39:00 +0900

    1. 컨트롤러 테스트 완료

--- src/main/java/com/jinan/profile/controller/board/BoardCommentController.java
+++ src/main/java/com/jinan/profile/controller/board/BoardCommentController.java
@@ -1,11 +1,13 @@
 package com.jinan.profile.controller.board;
 
 import com.jinan.profile.controller.board.request.BoardCommentRequest;
+import com.jinan.profile.controller.board.response.BoardCommentResponse;
 import com.jinan.profile.service.board.BoardCommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -18,9 +20,15 @@ public class BoardCommentController {
 
     /**
      * 게시글에 해당하는 댓글 조회
+     * 고민인게 이걸 ajax로 호출해서 아래에 뿌리도록 할까? 아니면 게시글을 조회할때 한번에 데이터를 동시에 가져오도록 할까?
      */
-    @GetMapping("/get")
-    public String getComment() {
+    @GetMapping("/get/{boardId}")
+    public String getComment(@PathVariable Long boardId, ModelMap modelMap) {
+
+//        BoardCommentResponse boardCommentResponse = boardCommentService.getBoardComment(boardId)
+//                .map(BoardCommentResponse::fromDto);
+
+        modelMap.addAttribute("comment", null);
         return null;
     }
 
--- src/test/java/com/jinan/profile/controller/board/BoardCommentControllerTest.java
+++ src/test/java/com/jinan/profile/controller/board/BoardCommentControllerTest.java
@@ -17,7 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("[Comment] - 댓글 컨트롤러 테스트")
@@ -31,10 +33,25 @@ class BoardCommentControllerTest extends ControllerTestSupport {
     @MockBean private UserService userService;
     @MockBean private PaginationService paginationService;
 
+    @DisplayName("게시글에 들어갔을때 그 게시글이 가진 댓글을 모두 가져온다.")
+    @Test
+    void selectBoardComment() throws Exception {
+        //given
+
+
+        //when & then
+        mockMvc.perform(get("/board/comment/get")
+                        .param("boardId", "1L")
+                )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("commentList"));
+
+
+    }
 
     @DisplayName("사용자가 댓글을 저장하면 댓글이 db에 저장된다.")
     @Test
-    void test() throws Exception {
+    void createComment() throws Exception {
         //given
         BoardCommentRequest boardCommentRequest = createBoardCommentRequest();
         String jsonResult = objectMapper.writeValueAsString(boardCommentRequest);
