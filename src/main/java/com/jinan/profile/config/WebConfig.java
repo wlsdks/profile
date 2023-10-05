75af92703a942d49d0e9d38c01fa05ca9763543e
wlsdks <wlsdks12@naver.com>
2023-10-05 11:33:28 +0900

    필요없는 webconfig 설정코드 제거

--- src/main/java/com/jinan/profile/config/WebConfig.java
+++ src/main/java/com/jinan/profile/config/WebConfig.java
@@ -39,23 +39,4 @@ public class WebConfig implements WebMvcConfigurer {
             "classpath:/META-INF/resources/webjars/"
     };
 
-    /**
-     * 뷰 컨트롤러를 추가할때 사용된다. 여기서는 루트 URL("/")에 접근했을 때 "/login"으로 리다이렉트하도록 설정하고 있다.
-     */
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        log.debug("[+] WebConfig Start !!! ");
-        registry.addRedirectViewController("/", "/login");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-
-    /**
-     * 정적 리소스를 처리하는 핸들러를 추가하는데 사용된다.
-     * 여기서는 모든 요청("/**")에 대해 CLASSPATH_RESOURCE_LOCATIONS에 지정된 경로에서 리소스를 찾도록 설정하고 있다.
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }
-
 }
